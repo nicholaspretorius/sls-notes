665ce20251dec505f3e350f60c5efb3531c9b4aa
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-07 11:55:40 +0200

    feat: Add logger

---
+++ api/src/utils/logger.js
@@ -0,0 +1,12 @@
+import * as winston from "winston";
+
+export function createLogger(loggerName) {
+  return winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    defaultMeta: {
+      name: loggerName
+    },
+    transports: [new winston.transports.Console()]
+  });
+}
