3b2849e231eadde30e4db3610b272732a145b77d
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-08 12:53:07 +0200

    feat: Add AWS Amplify

---
+++ client/src/config.js
@@ -0,0 +1,17 @@
+// AWS Amplify Config
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "sls-notes-uploads-bucket-"
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://lz2ymosryl.execute-api.eu-west-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_iqrXNOPhZ",
+    APP_CLIENT_ID: "4kmdbsn7s9va7lvslk1ntv48u9",
+    IDENTITY_POOL_ID: "eu-west-1:51eb39a2-6a02-47e6-8784-313ba2c7a288"
+  }
+};
