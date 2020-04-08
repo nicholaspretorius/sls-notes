f566fa1afd8f90fe20b8e02ad3a480789ac7ff5e
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-08 15:58:59 +0200

    feat: Add Login

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
-export default function Routes() {
+export default function Routes({ appProps }) {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
       <Route component={NotFound} />
     </Switch>
   );
