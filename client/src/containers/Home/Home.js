23d8091cad74311d41bf5a991b2e466cf4c8a65c
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-09 14:00:48 +0200

    feat: Add fetching notes

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,13 +1,56 @@
 import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { API } from "aws-amplify";
 import "./Home.css";
 
 export default function Home(props) {
   const [notes, setNotes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const { isAuthenticated } = props;
 
-  function renderNotesList() {
-    return null;
+  useEffect(() => {
+    async function onLoad() {
+      if (!isAuthenticated) {
+        return;
+      }
+
+      try {
+        const notes = await loadNotes();
+        setNotes(notes);
+      } catch (e) {
+        console.error(e);
+        // onError(e)
+      }
+
+      setIsLoading(false);
+    }
+
+    onLoad();
+  }, [isAuthenticated]);
+
+  function loadNotes() {
+    return API.get("notes", "/notes");
+  }
+
+  function renderNotesList(notes) {
+    return [{}].concat(notes).map((note, i) =>
+      i !== 0 ? (
+        <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
+          <ListGroupItem header={note.content && note.content.trim().split("\n")[0]}>
+            {"Created: " + new Date(note.createdAt).toLocaleString()}
+          </ListGroupItem>
+        </LinkContainer>
+      ) : (
+        <LinkContainer key="new" to="/notes/new">
+          <ListGroupItem>
+            <h4>
+              <b>{"\uFF0B"}</b> Create a new note
+            </h4>
+          </ListGroupItem>
+        </LinkContainer>
+      )
+    );
   }
 
   function renderLanding() {
@@ -28,5 +71,5 @@ export default function Home(props) {
     );
   }
 
-  return <div className="Home">{props.isAuthenticated ? renderNotes() : renderLanding()}</div>;
+  return <div className="Home">{isAuthenticated ? renderNotes() : renderLanding()}</div>;
 }
