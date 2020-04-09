0d5d8c40d1072a98bede05a99f17c194bd4f4ff0
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-09 13:36:56 +0200

    feat: Update Home for conditional rendering

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
 
-export default function Home() {
-  return (
-    <div className="Home">
+export default function Home(props) {
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function renderNotesList() {
+    return null;
+  }
+
+  function renderLanding() {
+    return (
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
       </div>
-    </div>
-  );
+    );
+  }
+
+  function renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>{!isLoading && renderNotesList(notes)}</ListGroup>
+      </div>
+    );
+  }
+
+  return <div className="Home">{props.isAuthenticated ? renderNotes() : renderLanding()}</div>;
 }
