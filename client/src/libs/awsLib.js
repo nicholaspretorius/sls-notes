57694b5d4565f57cee3f2795eeab06c717eebf74
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-09 15:07:33 +0200

    fix: Fix typo in filename

--- client/src/libs/awsLib.js
+++ client/src/libs/awsLib.js
@@ -1,7 +1,7 @@
 import { Storage } from "aws-amplify";
 
 export async function s3Upload(file) {
-  const filename = `${Date.now()}=${file.name}`;
+  const filename = `${Date.now()}-${file.name}`;
 
   // Upload the file to the user’s folder in S3 using the Storage.vault.put() object.
   // Alternatively, if we were uploading publicly you can use the Storage.put() method.
