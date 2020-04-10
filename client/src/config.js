4f20af6f39dce3efb5d6809bd6f6d0cb25570bfa
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-10 16:07:47 +0200

    chore: Update API and Cognito details

--- client/src/config.js
+++ client/src/config.js
@@ -7,12 +7,12 @@ export default {
   },
   apiGateway: {
     REGION: "eu-west-1",
-    URL: "https://lz2ymosryl.execute-api.eu-west-1.amazonaws.com/dev"
+    URL: "https://1eqsqxr7bj.execute-api.eu-west-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "eu-west-1",
-    USER_POOL_ID: "eu-west-1_SNPNfN0QV", // "eu-west-1_iqrXNOPhZ",
-    APP_CLIENT_ID: "1j7j70cbcujf5mpt3be1qqgfgc", // "4kmdbsn7s9va7lvslk1ntv48u9",
-    IDENTITY_POOL_ID: "eu-west-1:eed9a006-8d76-4fb6-a7cf-4c26bebb2720" // "eu-west-1:51eb39a2-6a02-47e6-8784-313ba2c7a288"
+    USER_POOL_ID: "eu-west-1_42dKV66ZZ", // "eu-west-1_iqrXNOPhZ",
+    APP_CLIENT_ID: "3qu4uneoimio7155vf2mqjvf43", // "4kmdbsn7s9va7lvslk1ntv48u9",
+    IDENTITY_POOL_ID: "eu-west-1:f9b775c2-88ee-4349-aab1-635cb5864230" // "eu-west-1:51eb39a2-6a02-47e6-8784-313ba2c7a288"
   }
 };
