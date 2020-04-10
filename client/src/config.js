d9c1912a403fb80d9b21e2eeecb4e6e61d562f2c
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-04-10 13:57:34 +0200

    feat: Change Cognito pools

--- client/src/config.js
+++ client/src/config.js
@@ -11,8 +11,8 @@ export default {
   },
   cognito: {
     REGION: "eu-west-1",
-    USER_POOL_ID: "eu-west-1_iqrXNOPhZ",
-    APP_CLIENT_ID: "4kmdbsn7s9va7lvslk1ntv48u9",
-    IDENTITY_POOL_ID: "eu-west-1:51eb39a2-6a02-47e6-8784-313ba2c7a288"
+    USER_POOL_ID: "eu-west-1_SNPNfN0QV", // "eu-west-1_iqrXNOPhZ",
+    APP_CLIENT_ID: "1j7j70cbcujf5mpt3be1qqgfgc", // "4kmdbsn7s9va7lvslk1ntv48u9",
+    IDENTITY_POOL_ID: "eu-west-1:eed9a006-8d76-4fb6-a7cf-4c26bebb2720" // "eu-west-1:51eb39a2-6a02-47e6-8784-313ba2c7a288"
   }
 };
